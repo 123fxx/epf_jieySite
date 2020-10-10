accd5350a647e3e7cecdc75cd17325a57a042fd8
fengpt <1378247557@qq.com>
2020-10-10 11:15:41 +0800

    弹窗公告无数据时不显示

--- src/home/main/main-make-up/advertising.vue
+++ src/home/main/main-make-up/advertising.vue
@@ -1,26 +1,23 @@
 <template>
-  <div class="advertising"
-       id="advertising"
-       @mouseover="over"
-       @mouseout="out"
-       v-if="$store.state.advertising">
-    <div class="advertising-child">
-      <p v-for="(item,index) in list"
-         :key="index"
-         style="cursor:pointer">
-        <span @click="toDetail(item)"> {{index+1}}{{item.name}}</span>
-
+  <div
+    class="advertising"
+    id="advertising"
+    @mouseover="over"
+    @mouseout="out"
+    v-if="$store.state.advertising"
+  >
+    <div class="advertising-child" v-if="list.length > 0">
+      <p v-for="(item, index) in list" :key="index" style="cursor: pointer">
+        <span @click="toDetail(item)"> {{ index + 1 }}{{ item.name }}</span>
       </p>
-
     </div>
-    <div class="close"
-         @click="close"></div>
+    <div class="close" @click="close" v-if="list.length > 0"></div>
   </div>
 </template>
 
 <script>
 export default {
-  data () {
+  data() {
     return {
       itl: null,
       advertising: true,
@@ -31,58 +28,55 @@ export default {
       xin: true,
       yin: true,
       ele: null,
-      list: []
-    }
+      list: [],
+    };
   },
-  created () {
-    this.getList()
+  created() {
+    this.getList();
   },
-  mounted () {
-    let that = this
+  mounted() {
+    let that = this;
     if (!this.$store.state.advertising) {
-      return
+      return;
     }
     this.ele = document.getElementById("advertising");
     // this.itl = window.setInterval(that.float, 20);
     this.itl = window.setInterval(function () {
-      that.float()
-    }, 20)
+      that.float();
+    }, 20);
   },
 
   methods: {
-    toDetail (item) {
+    toDetail(item) {
       const { href } = this.$router.resolve({
         path: `noticeDetail`,
-        query: { name: item.name, id: item.id }
+        query: { name: item.name, id: item.id },
       });
       window.open(href, "_blank");
     },
-    getList () {
-
-
-      this.$get(
-        '/ords/epfcms/cmsItem/queryCmsItemFloat',
-        {}
-      ).then(res => {
-        this.list = res.items
-
-      })
+    getList() {
+      this.$get("/ords/epfcms/cmsItem/queryCmsItemFloat", {}).then((res) => {
+        this.list = res.items;
+      });
     },
-    over () {
-      let that = this
+    over() {
+      let that = this;
       window.clearInterval(that.itl);
     },
-    out () {
-      let that = this
+    out() {
+      let that = this;
       this.itl = setInterval(that.float, that.delay);
     },
-    float () {
-
-      var L = 0; var T = 0;
+    float() {
+      var L = 0;
+      var T = 0;
       var R = document.documentElement.clientWidth - this.ele.offsetWidth;
       var B = document.documentElement.clientHeight - this.ele.offsetHeight;
-      this.ele.style.left = this.x1 + document.body.scrollLeft + 'px';
-      this.ele.style.top = this.y1 + document.body.scrollTop + 'px';
+      if (this.list.length > 0) {
+        this.ele.style.left = this.x1 + document.body.scrollLeft + "px";
+        this.ele.style.top = this.y1 + document.body.scrollTop + "px";
+      }
+
       this.x1 = this.x1 + this.step * (this.xin ? 1 : -1);
       if (this.x1 < L) {
         this.xin = true;
@@ -102,17 +96,17 @@ export default {
         this.y1 = B;
       }
     },
-    close () {
-      let that = this
-      this.$store.commit('close_advertising')
+    close() {
+      let that = this;
+      this.$store.commit("close_advertising");
       window.clearInterval(that.itl);
     },
   },
-  beforeDestroy () {
-    let that = this
+  beforeDestroy() {
+    let that = this;
     window.clearInterval(that.itl);
-  }
-}
+  },
+};
 </script>
 
 <style scoped>
